6e1c43a7ec8579fa12dbe09e9f823b6e092474cc
Mohammed <jnuolab@gmail.com>
2024-10-08 13:11:35 +0200

    changed main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:home_decor/screens/categories.dart';
 import 'package:home_decor/screens/filter.dart';
+import 'package:home_decor/screens/product.dart';
 import 'package:home_decor/screens/search.dart';
+import 'package:home_decor/screens/sub_category.dart';
 import 'package:home_decor/screens/user_reviews.dart';
 import 'package:home_decor/features/cart/screens/cart.dart';
 import 'package:home_decor/screens/profile.dart';
@@ -12,25 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
       // Tasks #1
       // home: FilterScreen(),
       // home: SearchScreen(),
       // Tasks #2
+      // home: CategoriesScreen(),
+      // home: SubCategory(),
+      // home: ProductScreen(),
+      home: UserReviews(),
       // Tasks #3
       // Tasks #4
-      // home: CartScreen(),
-      // home: CartDetails(),
-      // home: CheckoutScreen(),
-      // home: ThankYou(),
-      // home: OoopsScreen(),
-      // home: TrackOrder(),
-      // home: HelpAndFaqs(),
-      // home: HelpSearch(),
-      // home: CustomerService(),
-      // home: Assistant(),
-      // home: Profile(),
     );
   }
 }
